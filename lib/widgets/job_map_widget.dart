53b173b5804131f79cadd7daf7defa318f562ad2
YuanSamuel <samuel6161@gmail.com>
2022-01-30 09:31:36 -0600

    small fixes

--- lib/widgets/job_map_widget.dart
+++ lib/widgets/job_map_widget.dart
@@ -37,123 +37,129 @@ class _JobMapWidgetState extends State<JobMapWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
-      child: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => AcceptJobScreen(job: widget.job),
-            ),
-          );
-        },
-        child: Container(
-          width: width * 0.8,
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: height * 0.24,
-                  width: width * 0.8,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(25.0)),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: width * 0.05, vertical: height * 0.015),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => AcceptJobScreen(job: widget.job),
+                ),
+              );
+            },
+            child: Container(
+              width: width * 0.8,
+              height: height * 0.28,
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: height * 0.24,
+                      width: width * 0.8,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25.0)),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: width * 0.05, vertical: height * 0.015),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
                               children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Container(
+                                      child: Text(
+                                        StringHelper().getDateString(
+                                            widget.job.date.toDate()),
+                                        style: StyleConstants.subTextReg,
+                                      ),
+                                    ),
+                                    Text(
+                                      StringHelper()
+                                          .getTimeString(widget.job.date.toDate()),
+                                      style: StyleConstants.subTextReg,
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                            Spacer(),
+                            Row(
+                              children: [
+                                Text(
+                                  widget.job.name,
+                                  style: StyleConstants.titleTextBold,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                SizedBox(
+                                  width: width * 0.025,
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: height * 0.01,
+                            ),
+                            Row(
+                              children: [
+                                Icon(Icons.location_on, size: 15.0),
                                 Container(
+                                  width: width * 0.5,
                                   child: Text(
-                                    StringHelper()
-                                        .getDateString(widget.job.date.toDate()),
+                                    location,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
                                     style: StyleConstants.subTextReg,
                                   ),
                                 ),
-                                Text(
-                                  StringHelper()
-                                      .getTimeString(widget.job.date.toDate()),
-                                  style: StyleConstants.subTextReg,
-                                ),
                               ],
-                            )
-                          ],
-                        ),
-                        Spacer(),
-                        Row(
-                          children: [
-                            Text(
-                              widget.job.name,
-                              style: StyleConstants.titleTextBold,
                             ),
                             SizedBox(
-                              width: width * 0.025,
+                              height: height * 0.01,
                             ),
                           ],
                         ),
-                        SizedBox(
-                          height: height * 0.01,
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.location_on, size: 15.0),
-                            Container(
-                              width: width * 0.5,
-                              child: Text(
-                                location,
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: StyleConstants.subTextReg,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: height * 0.01,
-                        ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Positioned(
-                top: 0.0,
-                left: width * 0.04,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20.0),
-                  child: Container(
-                    height: width * 0.2,
-                    width: width * 0.2,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.white),
-                    // child: CachedNetworkImage(
-                    //   imageUrl: currentScan.pet.image != null &&
-                    //       currentScan.pet.image.isNotEmpty
-                    //       ? currentScan.pet.image
-                    //       : StyleConstants.defaultPetImageUrl,
-                    child: Image(
-                      image: organizer != null
-                          ? Image.network(organizer!.profileUrl).image
-                          : Image.network(
-                                  'https://firebasestorage.googleapis.com/v0/b/college-competitions.appspot.com/o/profilePictures%2FRectangle%20443.png?alt=media&token=a67e01cf-d7f2-4163-bd8d-5d9797743403')
-                              .image,
-                      fit: BoxFit.cover,
+                  Positioned(
+                    top: 0.0,
+                    left: width * 0.04,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Container(
+                        height: width * 0.2,
+                        width: width * 0.2,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Colors.white),
+                        // child: CachedNetworkImage(
+                        //   imageUrl: currentScan.pet.image != null &&
+                        //       currentScan.pet.image.isNotEmpty
+                        //       ? currentScan.pet.image
+                        //       : StyleConstants.defaultPetImageUrl,
+                        child: Image(
+                          image: organizer != null
+                              ? Image.network(organizer!.profileUrl).image
+                              : Image.network('https://firebasestorage.googleapis.com/v0/b/college-competitions.appspot.com/o/profilePictures%2FRectangle%20443.png?alt=media&token=a67e01cf-d7f2-4163-bd8d-5d9797743403').image,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          SizedBox(height: height * 0.05),
+        ],
       ),
     );
   }
